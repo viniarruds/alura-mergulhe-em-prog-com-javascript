1f8f8015920bcfc924c3c99307c2156030abc14a
viniarruds <viniarruds@gmail.com>
2024-01-14 17:47:04 -0300

    primeiro commit

---
+++ app.js
@@ -0,0 +1,33 @@
+alert('Boas vindas ao jogo do número secreto');
+let numeroMaximo = 50;
+let numeroSecreto = parseInt(Math.random() * numeroMaximo + 1);
+console.log(numeroSecreto);
+let chute;
+let tentativas = 1;
+
+//enquanto(while) chute não for igual(!=) ao número secreto
+while (chute != numeroSecreto) {
+    chute = prompt(`Escolha um número entre 1 e ${numeroMaximo}`);
+    //se(if) chute for igual ao número secreto //caso contrário(else)
+    if (chute == numeroSecreto) {
+        break;
+    } else {
+        if (chute > numeroSecreto) {
+            alert(`O número secreto é menor que ${chute}`);
+        } else {
+            alert(`O número secreto é maior que ${chute}`);
+        } 
+        //acrescentar mais um(++) em tentativas
+        tentativas++;
+    }
+}
+
+//caso tentativa for maior que um = tentativas, caso contrário tentativa
+let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa'
+alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} ${palavraTentativa}.`);
+
+//if (tentativas > 1) {
+//    alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativas.`);
+//} else {
+//    alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativa.`);
+//}
